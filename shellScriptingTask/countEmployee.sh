cf09cd1af658607226e2cb566c0ab7a412b9c1b3
Hany Salama <hanyslmm>
2019-02-15 20:53:31 +0200

    get total number of employees and print the result on the screen.

--- shellScriptingTask/countEmployee.sh
+++ shellScriptingTask/countEmployee.sh
@@ -1,19 +1,26 @@
 #!/bin/bash       
-#title           :reasCsb.sh
+#title           :countEmployee.sh
 #description     :This script to read csv file.
 #author		 :Hany Salama
 #date            :20190315
 #version         :0.1
+#usage		 :bash countEmployee.sh file.csv
 #=========================================================================
 echo "hello $(whoami)"
 echo "running this script in $(date +%F)"
-OLDIFS=$IFS
-while read Name Age PhoneNumber Email Salary Department CountryCode CountryCode
- do
-	echo -e "Name : $Name"
-	echo "========================"
-	echo "Age : $Age"
-	echo "hello there"
- done < $1
 
+INPUT=Employees.csv
+OLDIFS=$IFS
+IFS=","
+COUNTER=0
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read Name Age PhoneNumber Email Salary Department CountryCode CountryCode
+do
+	#echo "Name : $Name"
+	#echo "DOB : $Age"
+	#echo "===================="
+	COUNTER=$((COUNTER+1))
+done < $INPUT
+echo "=================================="
+echo "Number of Employees: $COUNTER "
 IFS=$OLDIFS
--- shellScriptingTask/read.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-OLDIFS=$IFS
-IFS=","
-while read Name Age
- do
-	echo -e "\e[1;33m$Name \
-	========================\e[0m\n\"
- done < $1
-IFS=$OLDIFS
